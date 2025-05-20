0d9773cb8d7c6b93b764ad11e15628af0194e844
MauricioChavesDias <mchavesdias@willowinc.com>
2025-05-20 15:33:14 +1000

    Initial Commit

---
+++ EldenRingWiki/EldenRingWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EldenRingWikiApp.swift
+//  EldenRingWiki
+//
+//  Created by Mauricio Chaves Dias on 20/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct EldenRingWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
